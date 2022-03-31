053ea35c893ef73fd830eef90db1e2cd282b64df
buttering <54806742+buttering@users.noreply.github.com>
2022-03-31 11:58:19 +0800

    调整项目结构

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, g
+from neo4j import GraphDatabase
+import config
 
 app = Flask(__name__)
+app.config.from_envvar()
 
+
+######## 路由操作
 
 @app.route('/')
 def hello_world():
@@ -10,12 +15,12 @@ def hello_world():
 
 @app.route('/graph', methods=['GET'])
 def graph():
-    return
+    return ''
 
 
 @app.route('/graph/query', methods=['POST'])
 def graph_query():
-    return
+    return ''
 
 
 @app.route('/user/sign', methods=['GET', 'POST', 'DELETE'])
@@ -26,6 +31,26 @@ def user_sign():
 @app.route('/user/register', methods=['GET', 'POST'])
 def user_register():
     return
+
+
+######## 数据库操作
+
+driver = GraphDatabase.driver(config.DATABASE_URL, auth=(config.DATABASE_USERNAME, config.DATABASE_PASSWORD))
+
+
+def get_db(self):
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = self.driver.session()
+    return g.neo4j_db
+
+
+# 程序上下文销毁钩子
+@app.teardown_appcontext
+def close_db(self, error):
+    if hasattr(g, 'neo4j_db'):
+        print('db close')
+        g.neo4j_db.close()
+
 
 
 if __name__ == '__main__':
